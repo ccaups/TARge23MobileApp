a7b781112c5ae6eb6f30a95ed8c22bfceee1b852
ccaups <ccaups@gmail.com>
2024-09-23 13:40:19 +0300

    CRUDdbContext

---
+++ MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiCRUD.ViewModels
+{
+    public class ProductsViewModel : ObservableObject
+    {
+
+    }
+}
